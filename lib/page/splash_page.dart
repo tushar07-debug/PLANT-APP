eba17464ca12692ebb235acb44883d226fab2839
Tushar Kumar Singh <tusharsingh102003@gmail.com>
2024-02-23 22:16:52 +0530

    first commit

---
+++ lib/page/splash_page.dart
@@ -0,0 +1,98 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:ui_13/core/color.dart';
+import 'package:ui_13/widgets/bottom_nav.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const SizedBox(height: 25),
+          const Text(
+            'GardenGrove',
+            style: TextStyle(
+              fontSize: 50.0,
+              color: Colors.green,
+              letterSpacing: 1.8,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          const SizedBox(height: 5),
+          const Text(
+            'Make EARTH Green',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              letterSpacing: 1.8,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 450,
+            width: 450,
+            child: Image.asset('assets/images/03.png'),
+            // child: Image.asset('assets/images/Asset1.png'),
+          ),
+          const SizedBox(height: 25),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (builder) => const BottomNavBar()));
+            },
+
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 80.0,
+                vertical: 12.0,
+              ),
+              decoration: BoxDecoration(
+                color: green,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: const Text(
+                'Sign In',
+                style: TextStyle(
+                  color: white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ),
+
+          ),
+
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Create an account',
+              style: TextStyle(
+                color: black,
+                fontSize: 16,
+                letterSpacing: 1,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Forgot Password?',
+              style: TextStyle(
+                color: black.withOpacity(0.4),
+                letterSpacing: 1,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
